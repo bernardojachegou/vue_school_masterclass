855d13dd6db90937f021749043c798b6f869ff26
Michel Bernardo <bernardoenglish@outlook.com>
2020-11-27 20:19:07 -0400

    adding scoped css styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    <h1>Welcome to my forum</h1>
-    <div v-for="thread in threads" v-bind:key="thread">
-      <h2>{{ thread.title }}</h2>
-      <div v-for="postId in thread.posts" v-bind:key="postId">
-        <p>{{ users[posts[postId]] }}</p>
-        <p>{{ posts[postId].text }}</p>
+    <div
+      v-for="thread in threads"
+      v-bind:key="thread"
+      class="col-large push-top"
+    >
+      <h1>{{ thread.title }}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts" v-bind:key="postId" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{
+              users[posts[postId].userId].name
+            }}</a>
+
+            <a href="#">
+              <img
+                class="avatar-large"
+                :src="users[posts[postId].userId].avatar"
+                alt=""
+              />
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>
+              {{ posts[postId].text }}
+            </div>
+          </div>
+
+          <div class="post-date text-faded">
+            {{ posts[postId].publishedAt }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -13,7 +42,6 @@
 
 <script>
 import sourceData from "@/data";
-console.log(sourceData);
 
 export default {
   name: "HelloWorld",
