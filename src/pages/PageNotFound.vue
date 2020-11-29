22d64dc573754686a8da53d19a6c4a04338de5aa
Michel Bernardo <bernardoenglish@outlook.com>
2020-11-29 06:32:38 -0400

    1. Using router-link to creating routing avoding realoads; 2. Renaming components to be different from pages, a page component should start with PageComponentName; 3. Using absolute path to components, it can be used and we can create a alias to set new paths; 4. Creating a NotFound page and using style module in it. It's a good practice to set unique styles;

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-full" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not Found</h1>
+    <p :class="$style.textLarge">
+      Oops, We can't track you anymore,
+      <router-link :to="{ name: 'Home' }"> try back home! </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style module>
+.headingLarge {
+  font-size: 80px;
+}
+
+.textLarge {
+  font-size: 40px;
+  font-weight: 100;
+}
+
+.centered {
+  text-align: center;
+}
+</style>
