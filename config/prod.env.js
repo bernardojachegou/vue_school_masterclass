51f23eff5e1795723218770400066beb044b0716
Michel Bernardo <bernardoenglish@outlook.com>
2021-02-07 22:58:22 -0400

    adding env lib to carry about the security of the db

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,15 @@
 "use strict";
+require("dotenv").config();
+
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_apiKey: '"AIzaSyApfd9e7ZCqZKhzsRNB5MntMZPvYAoJGxg"',
-  FIREBASE_authDomain: '"vue-school-forum-ecc88.firebaseapp.com"',
-  FIREBASE_projectId: '"vue-school-forum-ecc88"',
-  FIREBASE_storageBucket: '"vue-school-forum-ecc88.appspot.com"',
-  FIREBASE_messagingSenderId: '"981864202775"',
-  FIREBASE_appId: '"1:981864202775:web:aea807a6705d1b5d06d252"',
-  FIREBASE_measurementId: '"G-XZJ6RY9KK3"'
+  NODE_ENV: '"production"',
+  FIREBASE_apiKey: JSON.stringify(process.env.FIREBASE_apiKey),
+  FIREBASE_authDomain: JSON.stringify(process.env.FIREBASE_authDomain),
+  FIREBASE_projectId: JSON.stringify(process.env.FIREBASE_projectId),
+  FIREBASE_storageBucket: JSON.stringify(process.env.FIREBASE_storageBucket),
+  FIREBASE_messagingSenderId: JSON.stringify(
+    process.env.FIREBASE_messagingSenderId
+  ),
+  FIREBASE_appId: JSON.stringify(process.env.FIREBASE_appId),
+  FIREBASE_measurementId: JSON.stringify(process.env.FIREBASE_measurementId)
 };
