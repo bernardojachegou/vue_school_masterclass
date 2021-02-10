661b8914425766ca82f4b940f169d3533dc4a417
Michel Bernardo <bernardoenglish@outlook.com>
2021-02-09 22:56:20 -0400

    adding the action to create the user and push it into the database, got some problem related to child in the method

--- src/store/actions.js
+++ src/store/actions.js
@@ -127,7 +127,7 @@ export default {
     });
   },
 
-  createUser({ state, commit }, { email, name, username, avatar = null }) {
+  createUser({ state, commit }, { id, email, name, username, avatar = null }) {
     return new Promise((resolve, reject) => {
       const registeredAt = Math.floor(Date.now() / 1000);
       const usernameLower = username.toLowerCase();
@@ -142,21 +142,35 @@ export default {
         registeredAt
       };
 
-      const userId = firebase
-        .database()
-        .ref("users")
-        .push().key;
-
       firebase
         .database()
         .ref("users")
-        .child(userId)
+        .child(id)
         .set(user)
         .then(() => {
-          commit("setItem", { resource: "users", id: userId, item: user });
-          resolve(state.users[userId]);
+          commit("setItem", { resource: "users", id: id, item: user });
+          resolve(state.users[id]);
         });
     });
+  },
+
+  registerUserWithEmailAndPassword(
+    { dispatch },
+    { email, name, username, password, avatar = null }
+  ) {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        return dispatch("createUser", {
+          id: user.uid,
+          email,
+          name,
+          username,
+          password,
+          avatar
+        });
+      });
   },
 
   updateThread({ state, commit, dispatch }, { title, text, id }) {
