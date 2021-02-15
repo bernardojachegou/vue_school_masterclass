1f61f9e42b758c50c769aab8313e2c76efc07c22
Michel Bernardo <bernardoenglish@outlook.com>
2021-02-15 09:38:09 -0400

    adding the click-outside to toggle the profile menu options

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -14,7 +14,7 @@
     <!-- use .navbar-open to open nav -->
     <nav class="navbar">
       <ul v-if="user">
-        <li class="navbar-user">
+        <li class="navbar-user" v-click-outside="closeUserDropdown">
           <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="user.avatar" alt="" />
             <span>
@@ -57,16 +57,29 @@
 
 <script>
 import { mapGetters } from "vuex";
+import clickOutside from "@/directives/click-outside";
+
 export default {
+  directives: {
+    clickOutside,
+  },
+
   data() {
     return {
       userDropdownOpen: false,
     };
   },
+
   computed: {
     ...mapGetters({
       user: "auth/authUser",
     }),
+  },
+
+  methods: {
+    closeUserDropdown() {
+      this.userDropdownOpen = false;
+    },
   },
 };
 </script>
